51a35b048f5d64104599d5214c2b0615036f14dd
Clayman <Clayman@DESKTOP-DM8E77U>
2023-04-15 13:21:31 +0300

    Operators day 1

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 using namespace std;
 
+#define delimiter "\n--------------------------------------------\n"
+
 /*
 ---------------
 Создавая структуру или класс, мы создаем новый тип данных,
@@ -52,19 +54,53 @@ public:
 		this->y = y;
 		cout << "Constructor:\t\t" << this << endl;
 	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	~Point()
 	{
 		cout << "Destructor:\t\t" << this << endl;
 	}
 
+	//				Operators:
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
 	//				Methods:
+	double distance(const Point& other)const//Point other - другой объект (другая точка)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+		return distance;
+	}
+
 	void print()const
 	{
 		cout << "X = " << x << "\tY = " << y << endl;
 	}
 };
 
+double distance(const Point& A, const Point& B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+	return distance;
+}
+
 //#define STRUCT_POINT
+//#define CONSTRUCTORS_CHECK
+//#define DISTANCE_CHECK
+//#define ASSIGNMENT_CHECK
 
 void main()
 {
@@ -85,17 +121,58 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
 
+#ifdef CONSTRUCTORS_CHECK
 	Point A;	//Default constructor
-	//A.set_x(2);
-	//A.set_y(3);
-	//cout << A.get_x() << "\t" << A.get_y() << endl;
+//A.set_x(2);
+//A.set_y(3);
+//cout << A.get_x() << "\t" << A.get_y() << endl;
 	A.print();
 
-	Point B = 5;
+	Point B = 5;	//Single-argument constructor
 	B.print();
 
 	Point C(2, 3);
 	C.print();
+
+	Point D = C;	//Copy constructor (Deep copy) new
+					//Shallow copy
+	D.print();
+
+	Point E;
+	E = D;		//Copy assignment (оператор присваивания)
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef DISTANCE_CHECK
+	Point A(2, 3);
+	A.print();
+
+	Point B(7, 8);
+	B.print();
+	cout << delimiter << endl;
+	cout << "Расстояние от точки A до точки B: " << A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки A до точки B: " << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками A и B:   " << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками B и A:   " << distance(B, A) << endl;
+	cout << delimiter << endl;
+#endif // DISTANCE_CHECK
+
+#ifdef ASSIGNMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
 }
 
 /*
@@ -137,5 +214,35 @@ void main()
 	-по умолчанию - это конструктор, который может быть вызван без параметров;
 	-конструктор копирования;
 	-конструктор переноса;
+------------------------------------
+*/
+
+/*
+------------------------------------
+				Overloading rules:
+1. Перегрузить можно только существующие операторы.
+   Создавать новые операторы невозможно.
+	+  перегружается;
+	++ перегружается;
+	%  перегружается;
+	%% НЕ перегружается;
+	@  НЕ перегружфется;
+2. НЕ все существующие операторы можно перегрузить.
+   НЕ перегружаеются:
+	?: - Conditional ternary operator
+	:: - Scope operator (Оператор разрешения видимости)
+	.  - Point operator (Оператор прямого доступа)
+	.* - Pointer to member selection
+	#
+	##
+3. Перегруженные операторы сохраняют приоритет;
+4. Изменить поведение операторов над встроенными типами невозможно;
+------------------------------------
+	type operator@(parameters)
+	{
+		...;
+		group-of-statements;
+		...;
+	}
 ------------------------------------
 */
